0850a04865c9d65bacdd3a43310e535428f86629
Matt Moore <matt@mattmoore.io>
2020-04-21 07:49:16 -0500

    Try example from Scala site.

--- src/main/scala/io/mattmoore/CompilerPlugin.scala
+++ src/main/scala/io/mattmoore/CompilerPlugin.scala
@@ -1,46 +1,37 @@
 package io.mattmoore
 
-import scala.tools.nsc.{Global, Phase}
-import scala.tools.nsc.plugins._
-import scala.tools.nsc.transform._
+import scala.tools.nsc
+import nsc.Global
+import nsc.Phase
+import nsc.plugins.Plugin
+import nsc.plugins.PluginComponent
 
-class CompilerPlugin(override val global: Global) extends Plugin {
+class CompilerPlugin(val global: Global) extends Plugin {
 
-  override val name: String = "scala-compiler-plugin"
-  override val description: String = "scala compiler plugin simple example"
-
-  override val components: List[PluginComponent] = List(new CompilerPluginComponent(global))
-}
-
-class CompilerPluginComponent(val global: Global) extends PluginComponent with TypingTransformers {
   import global._
-  override val phaseName: String = "compiler-plugin-phase"
-  override val runsAfter: List[String] = List("parser")
 
-  override def newPhase(prev: Phase): Phase = new StdPhase(prev) {
-    override def apply(unit: global.CompilationUnit): Unit = {
-      unit.body = new MyTypingTransformer(unit).transform(unit.body)
+  val name = "divbyzero"
+  val description = "checks for division by zero"
+  val components = List[PluginComponent](Component)
+
+  private object Component extends PluginComponent {
+    val global: CompilerPlugin.this.global.type = CompilerPlugin.this.global
+    val runsAfter = List[String]("refchecks")
+    val phaseName = CompilerPlugin.this.name
+
+    def newPhase(_prev: Phase) = new DivByZeroPhase(_prev)
+
+    class DivByZeroPhase(prev: Phase) extends StdPhase(prev) {
+      override def name = CompilerPlugin.this.name
+
+      def apply(unit: CompilationUnit): Unit = {
+        for (tree@Apply(Select(rcvr, nme.DIV), List(Literal(Constant(0)))) <- unit.body
+             if rcvr.tpe <:< definitions.IntClass.tpe) {
+          global.reporter.error(tree.pos, "definitely division by zero")
+        }
+      }
     }
+
   }
 
-  class MyTypingTransformer(unit: CompilationUnit) extends TypingTransformer(unit) {
-    override def transform(tree: Tree) = tree match {
-      case dd: DefDef if (dd.mods.annotations.size > 0) =>
-        println(dd)
-        val ddd = treeCopy.DefDef(dd, dd.mods, dd.name,
-          dd.tparams, dd.vparamss, dd.tpt, Block(
-            q"""println("Inside - before")""",
-            DefDef(Modifiers(), TermName("runMethod"), List(),
-              List(), TypeTree(), dd.rhs),
-            q"val r = runMethod",
-            q"""println("Inside - after")""",
-            q"r"
-          ))
-        println(ddd)
-        ddd
-      case _ => super.transform(tree)
-    }
-  }
-  def newTransformer(unit: CompilationUnit) =
-    new MyTypingTransformer(unit)
 }
